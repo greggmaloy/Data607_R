169dc26ca1c6cc691fe91e9d9a9c6e330b97170c
goygoyummm <greggmaloy@gmail.com>
2023-02-04 14:08:25 -0500

    Delete Data_607_Assignment_2.sql

--- Data_607_Assignment_2.sql
+++
@@ -1,7 +0,0 @@
-update MovieSurvey set
- field7 = case when field7 = 'Did not see' then null else field7 end,
- field8 = case when field8 = 'Did not see' then null else field8 end,
- field9 = case when field9 = 'Did not see' then null else field9 end,
- field10 = case when field10= 'Did not see' then null else field10 end, 
- field11 = case when field11 = 'Did not see' then null else field11 end,
- field12 = case when field12 = 'Did not see' then null else field12 end
